a72072a76f246162c98cc8a902c585f9d101525f
lfsanchezg <felipe@Maca.local>
2013-11-23 11:58:24 -0300

    Clases de prueba para crear las carpetas

---
+++ Whisky/Whisky-ejb/src/java/sessionBeans/NewSessionBean.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sessionBeans;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author felipe
+ */
+@Stateless
+@LocalBean
+public class NewSessionBean {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+}
